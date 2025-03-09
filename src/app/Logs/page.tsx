31c02059088fde59c77c669e7f8e5e021d868c8b
Rafael Velazco <rjvelazco21@gmail.com>
2025-03-09 19:12:42 -0400

    refactor: swap Blog and Logs page components

--- src/app/Logs/page.tsx
+++ src/app/Logs/page.tsx
@@ -1,8 +1,8 @@
 
-export default function Blog() {
+export default function Logs() {
   return (
     <div className="max-w-4xl bg-background mx-auto font-sans flex flex-col gap-12 px-4 sm:px-6 py-8 md:py-12">
-        <h1 className="text-4xl md:text-5xl font-bold mb-12">Blog</h1>
+        <h1 className="text-4xl md:text-5xl font-bold mb-12">Logs</h1>
     </div>
   );
 }
