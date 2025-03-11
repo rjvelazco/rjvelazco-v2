26aefaf2a075b28bd9ded5b0f02d3d5485be8181
Rafael Velazco <rjvelazco21@gmail.com>
2025-03-10 22:45:45 -0400

    refactor: improve layout structure and simplify global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
 
 import localFont from "next/font/local";
-import Image from "next/image";
 
 import { NavBar } from "../layout/NavBar";
 
-import photo from "../../public/me.jpg";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -32,21 +30,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-paperWhite`}
+        className={`${geistSans.variable} ${geistMono.variable} layout_container`}
       >
-        <div className="w-full mx-auto max-w-[64rem] px-4 sm:px-6">
-          <header className="py-6 flex items-center justify-between">
-            <div className="flex items-center gap-4">
-              <div>
-                <h2 className="text-3xl font-bold">Rafael Velazco</h2>
-                <p className="text-lg text-gray-500">Senior Software Engineer</p>
-              </div>
+        {/* This should be a component */}
+        <header className="flex items-center justify-between py-6 w-full mx-auto max-w-[64rem]">
+          <div className="flex items-center gap-4">
+            <div>
+              <h2 className="text-3xl font-bold">Rafael Velazco</h2>
+              <p className="text-lg text-gray-500">Senior Software Engineer</p>
             </div>
-            <NavBar />
-          </header>
-
-          <main className="flex-grow">{children}</main>
-        </div>
+          </div>
+          <NavBar />
+        </header>
+        <main className="w-full mx-auto max-w-[64rem]">{children}</main>
         <footer className="py-6 flex items-center justify-center bg-gray-100">
           <p className="text-sm text-gray-500">
             &copy; {new Date().getFullYear()} Rafael Velazco. All rights reserved.
