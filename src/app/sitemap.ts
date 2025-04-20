06dfff4f359caae1f09cb6ed41a7c01657d4bc7f
Rafael Velazco <rjvelazco21@gmail.com>
2025-04-20 10:45:49 -0400

    feat: add robots and sitemap generation functionality, and introduce utility for fetching blog post paths

---
+++ src/app/sitemap.ts
@@ -0,0 +1,22 @@
+import { getPostPaths } from '@utils/getPostPaths';
+import { MetadataRoute } from 'next';
+
+export const baseUrl = 'https://www.rjvelazco.com';
+export const dynamic = 'force-static';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  // Hardcoded blog posts for static export
+  const postPaths = await getPostPaths();
+  const blogRoutes = postPaths.map((postPath) => ({
+    url: `${baseUrl}/blog/posts/${postPath.params.slug}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+
+  // Main routes
+  const routes = ['', '/blog', '/portfolio', '/logs'].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+
+  return [...routes, ...blogRoutes];
+}
