22ccb8ec89145ada5c427e1c22af30b1826f0ce4
Rafael Velazco <rjvelazco21@gmail.com>
2025-04-20 11:08:05 -0400

    refactor: simplify robots.txt generation by removing environment variable dependencies and hardcoding sitemap URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,19 +3,11 @@ import type { MetadataRoute } from 'next'
 export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_DOMAIN || 'https://www.rjvelazco.com';
-  const isNoindex = process.env.APP_INDEX_MODE === 'NOINDEX'
-
   return {
-    rules: isNoindex
-      ? {
-          userAgent: '*',
-          disallow: '/',
-        }
-      : {
-          userAgent: '*',
-          allow: '/',
-        },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://www.rjvelazco.com/sitemap.xml',
   }
 }
