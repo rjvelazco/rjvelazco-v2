87e074f7f408b60ca04813913b4350f1a34a2043
Rafael Velazco <rjvelazco21@gmail.com>
2025-08-24 20:14:59 -0400

    Update mdx-components.tsx

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,6 +1,6 @@
 import type { MDXComponents } from 'mdx/types';
 
-// Define custom MDX components here
+// Define custom MDX components here.
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components
