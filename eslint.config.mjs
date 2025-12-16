08ae379fc7768643073aa4dab252c365b95c27d6
Rafael Velazco <rjvelazco21@gmail.com>
2025-12-16 16:44:36 -0400

    chore: add Prettier configuration and update ESLint settings to integrate with Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
+  // Disable formatting-related ESLint rules that conflict with Prettier.
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
